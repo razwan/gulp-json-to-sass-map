ee95bdebc657beb449050633f3ef5c7f0b8bf8ce
Razvan <razwan.me@gmail.com>
2019-04-08 13:16:06 +0300

    replace deprecated gulp-util with plugin-error and remove unused lodash dependency

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 'use strict';
-var gutil = require('gulp-util');
-var through = require('through2');
-var fs = require('fs');
-var jsonToSass = require('json-to-sass-map');
-var _ = require('lodash');
+var through = require( 'through2' );
+var fs = require( 'fs' );
+var jsonToSass = require( 'json-to-sass-map' );
+var PluginError = require( 'plugin-error' );
 
-module.exports = function (options) {
-    return through.obj(function (file, enc, cb) {
+module.exports = function( options ) {
+	return through.obj( function( file, enc, cb ) {
 
-        if (file.isNull()) {
-            cb(null, file);
-            return;
-        }
+		if ( file.isNull() ) {
+			cb( null, file );
+			return;
+		}
 
-        if (file.isStream()) {
-            cb(new gutil.PluginError('gulp-json-to-sass', 'Streaming not supported'));
-            return;
-        }
+		if ( file.isStream() ) {
+			cb( new PluginError( 'gulp-json-to-sass', 'Streaming not supported' ) );
+			return;
+		}
 
 
-        try {
-            var jsonData = fs.readFileSync(options.source);
-            var scss = jsonToSass(jsonData);
-            fs.writeFileSync(options.output, scss);
-            this.push(file);
-        } catch (err) {
-            this.emit('error', new gutil.PluginError('gulp-temp', err));
-        }
+		try {
+			var jsonData = fs.readFileSync( options.source );
+			var scss = jsonToSass( jsonData );
+			fs.writeFileSync( options.output, scss );
+			this.push( file );
+		} catch ( err ) {
+			this.emit( 'error', new PluginError( 'gulp-temp', err ) );
+		}
 
-        cb();
-    });
+		cb();
+	} );
 };
